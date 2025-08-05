3802173464f42b4705359bc0c6b1a0a3908b9902
SW <sw@192.168.1.21>
2025-08-05 07:25:46 -0700

    updated ios calling

--- lib/webservices/sumbit_call_logs_api.dart
+++ lib/webservices/sumbit_call_logs_api.dart
@@ -5,12 +5,32 @@ extension SumbitCallLogsApi on WebService {
       {required String status,
       required String leadId,
       required String callDuration,
-      required String notes}) async {
+      required String notes,required String callStatus,required String date,required TimeOfDay time}) async {
+
+    String? format(String? dateStr, TimeOfDay? time) {
+      if (dateStr == null || time == null) return null;
+
+      try {
+        final DateFormat dateFormat = DateFormat('d MMMM y');
+        DateTime date = dateFormat.parse(dateStr);
+
+        final combinedDateTime = DateTime(date.year, date.month, date.day, time.hour, time.minute);
+
+        return DateFormat("yyyy-MM-dd'T'HH:mm:ss").format(combinedDateTime);
+      } catch (e) {
+        print('Date parsing error: $e');
+        return null;
+      }
+    }
+    String? dateTime = format(date,time); // both could be null
+
     Map<String, dynamic> params = {
-      "status":status,
-      "lead_id":leadId,
-      "call_duration":callDuration,
-      "notes":notes
+      "status": status,
+      "lead_id": leadId,
+      "call_duration": callDuration,
+      "notes": notes,
+      "call_status": callStatus,
+      if (dateTime != null) "date_time": dateTime, // include only if not null
     };
 
     final apiResponse =
